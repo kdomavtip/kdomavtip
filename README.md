02d2ea4895bfc2521ac360a02be6d46237a54942
Filip Peterek <fpeterek@seznam.cz>
2020-03-19 19:12:50 +0100

    Kdo ma vtip?

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# kdomavtip
+# Kdo má vtip?
+
 Kdo má vtip na dnešním stand-upu?
+
+---
+
+Stand-up se chýlí ke konci, všichni jsme natěšení na vtip, ale když na něj
+přijde řada, nikdo nemá vtip nachystaný. Musí se hledat vtip na Seznamu, ten má
+ve většině případů pochybnou kvalitu. Vývojáří jsou následkem špatného vtipu
+zklamaní, demotivovaní, a kvalita i efektivita jejich práce klesá.
+
+### Nikdy víc
+
+Díky službě [Kdo má vtip](https://kdomavtip.github.io) předem všichni víme, kdo
+si má na stand-up připravit vtip. Služba je dostupná i zvenčí, není pro ni třeba
+přístup do firemní sítě a informace, kdo si má na stand-up připravit vtip, je
+dostupná již od půlnoci. Nemůže se tedy stát, že si nikdo nepřipraví vtip.
+A pokud by se to náhodou stalo, tým alespoň ví, který člen nedostatek vtipu
+způsobil. Tým tedy může zdupat viníka, a špatně se bude cítit pouze člověk,
+který si vtip zapomněl připravit.
+
---
+++ script.js
@@ -0,0 +1,115 @@
+// Skript Jaromíra Soukupa
+// Kdo má dnes vtip
+
+let names = ['Dominik', 'Eliška', 'Filip', 'Michal', 'Vláďa', 'Wendigo']
+let days = ['neděle', 'pondělí', 'úterý', 'středa', 'čtvrtek', 'pátek', 'sobota']
+
+/* Copied from SO */
+
+Date.prototype.isLeapYear = function() {
+    var year = this.getFullYear();
+    if((year & 3) != 0) return false;
+    return ((year % 100) != 0 || (year % 400) == 0);
+};
+
+Date.prototype.getDayOfYear = function() {
+    var dayCount = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
+    var mn = this.getMonth();
+    var dn = this.getDate();
+    var dayOfYear = dayCount[mn] + dn;
+    if(mn > 1 && this.isLeapYear()) dayOfYear++;
+    return dayOfYear;
+};
+
+/* Not copied from SO */
+
+function date() {
+    let d = new Date()
+    /* For testing purposes */
+    // d.setDate(d.getDate() + 1)
+    return d
+}
+
+function dom() {
+    return date().getDate() - 1
+}
+
+function dow() {
+    return date().getDay()
+}
+
+function get_offset() {
+
+    if (dow() == 6) {
+        return 2
+    } else if (dow() == 0) {
+        return 1
+    }
+
+    return 0
+}
+
+function offset_date(offset) {
+    let d = date()
+    d.setDate(d.getDate() + offset)
+    return d
+}
+
+function update_jokebearer(text) {
+    document.getElementById('jokebearer').innerHTML = text
+}
+
+function update_dow_text(text) {
+    document.getElementById('day_of_week').innerHTML = text
+}
+
+function is_weekend(day_of_week) {
+    return day_of_week == 0 || day_of_week == 6
+}
+
+function get_dow_text() {
+
+    let day_of_week = dow()
+    let day = days[day_of_week]
+    let weekend_text = ', příští stand-up bude v pondělí'
+    let text = 'Dnes je ' + day + (is_weekend(day_of_week) ? weekend_text : '')
+
+    return text
+
+}
+
+function update_bg_if_filip(joker) {
+
+    if (joker != 'Filip') {
+        return
+    }
+
+    document.getElementById('joke').style.color = '#ff196e'
+    document.getElementById('joke').style.backgroundImage = "url('filip.jpg')"
+
+}
+
+function get_joke_text() {
+
+    let next_standup = offset_date(get_offset())
+
+    let weekend_text = 'Vtip bude mít '
+    let weekday_text = 'Vtip má '
+
+    let text = is_weekend(dow()) ? weekend_text : weekday_text
+
+    let joker = names[next_standup.getDayOfYear() % names.length]
+
+    update_bg_if_filip(joker)
+
+    return text + '<b id="name">' + joker + '</b>'
+}
+
+
+function calc() {
+
+    update_dow_text(get_dow_text())
+    update_jokebearer(get_joke_text())
+
+}
+
