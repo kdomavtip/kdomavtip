daad8665905a35ff4aa6cee0083a75474dfd66d8
Filip Peterek <fpeterek@seznam.cz>
2020-06-16 06:21:31 +0200

    Removed Ondra

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Skript Jaromíra Soukupa
 // Kdo má dnes vtip
 
-let NAMES = ['Ondra', 'Vláďa', 'Wendigo', 'Dominik', 'Eliška', 'Filip', 'Michal']
+let NAMES = ['Vláďa', 'Wendigo', 'Dominik', 'Eliška', 'Filip', 'Michal']
 let DAYS = ['pondělí', 'úterý', 'středa', 'čtvrtek', 'pátek', 'sobota', 'neděle']
-const EPOCH_START = new Date('2020-05-18T00:00:00');
+const EPOCH_START = new Date('2020-06-16T00:00:00');
 const NOW = new Date();
 // const NOW = new Date('2020-05-27T09:00:00'); // for DBG
 
